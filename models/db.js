73f450e9dd3bdaf3add1549f24e34b1475d23ef7
J-Marvin <60575288+J-Marvin@users.noreply.github.com>
2021-07-24 17:32:39 +0800

    Revamped db

--- models/db.js
+++ models/db.js
@@ -7,37 +7,50 @@ const database = {
     // opens the file and verbose prints the statements executed
     const db = sqlite3(path.join(folder, file), { verbose: console.log })
 
+    /* This statement creates the Baptismal Registry table
+       Fields: 
+       reg_id - primary id
+       date - date of the baptism
+       location - location of the baptism
+       officiant - the officiant of the baptism
+    */
     const createBapReg =
       'CREATE TABLE IF NOT EXISTS bap_reg (' +
         'reg_id INTEGER NOT NULL PRIMARY KEY AUTOINCREMENT, ' +
-        'date DATE, ' +
+        'date TEXT, ' +
         'location TEXT, ' +
         'officiant TEXT' +
         ')'
 
+    /* This statement create the Infant Dedication table 
+       Fields:
+       dedication_id - primary id
+       person_id - an id referencing the person(infant)
+       date - the date of the event
+       place - the place of the event
+       officiant - the officiant 
+    */
     const createInfDedication =
       'CREATE TABLE IF NOT EXISTS inf_dedication (' +
         'dedication_id INTEGER NOT NULL PRIMARY KEY AUTOINCREMENT, ' +
-        'first_name TEXT,' +
-        'middle_name TEXT,' +
-        'last_name TEXT, ' +
-        'date DATE, ' +
+        'person_id INTEGER NOT NULL, ' +
+        'parents_id INTEGER, ' +
+        'date TEXT, ' +
         'place TEXT, ' +
-        'officiant TEXT' +
+        'officiant TEXT,' +
+        'FOREIGN KEY(person_id) REFERENCES people(person_id),' +
+        'FOREIGN KEY(parents_id) REFERENCES couples(couple_id)' +
       ')'
 
     const createWitness =
       'CREATE TABLE IF NOT EXISTS witness (' +
         'witness_id INTEGER NOT NULL PRIMARY KEY AUTOINCREMENT, ' +
         'dedication_id INTEGER,' +
-        'parent_1_id INTEGER,' +
-        'parent_2_id INTEGER,' +
-        'first_name TEXT,' +
-        'middle_name TEXT,' +
-        'last_name TEXT,' +
+        'wedding_id INTEGER,' +
+        'person_id INTEGER NOT NULL,' +
         'FOREIGN KEY(dedication_id) REFERENCES inf_dedication(dedication_id),' +
-        'FOREIGN KEY(parent_1_id) REFERENCES members(member_id),' +
-        'FOREIGN KEY(parent_2_id) REFERENCES members(member_id)' +
+        'FOREIGN KEY(wedding_id) REFERENCES wedding_red(reg_id),' + 
+        'FOREIGN KEY(person_id) REFERENCES people(person_id)' +
       ')'
 
     const createAccounts =
@@ -49,27 +62,19 @@ const database = {
     const createPreNuptial =
       'CREATE TABLE IF NOT EXISTS pre_nuptial (' +
         'record_id INTEGER NOT NULL PRIMARY KEY AUTOINCREMENT,' +
-        'bride_first_name TEXT, ' +
-        'bride_maiden_name TEXT,' +
-        'bride_last_name TEXT,' +
-        'groom_first_name TEXT, ' +
-        'groom_maiden_name TEXT,' +
-        'groom_last_name TEXT,' +
-        'date DATE,' +
-        'date_of_wedding DATE' +
+        'couple_id INTEGER NOT NULL,' +
+        'date TEXT,' +
+        'date_of_wedding TEXT,' +
+        'FOREIGN KEY(couple_id) REFERENCES couples(couple_id)' +
       ')'
 
     const createWeddingReg =
       'CREATE TABLE IF NOT EXISTS wedding_reg (' +
         'reg_id INTEGER NOT NULL PRIMARY KEY AUTOINCREMENT,' +
-        'prenup_record_id INTEGER,' +
-        'bride_first_name TEXT, ' +
-        'bride_maiden_name TEXT,' +
-        'bride_last_name TEXT,' +
-        'groom_first_name TEXT, ' +
-        'groom_maiden_name TEXT,' +
-        'groom_last_name TEXT,' +
-        'date DATE,' +
+        'prenup_record_id INTEGER NOT NULL,' +
+        'bride_parents_id INTEGER,' +
+        'groom_parents_id INTEGER,' +
+        'date TEXT,' +
         'location TEXT, ' +
         'solemnizing_officer TEXT,' +
         'contract_no TEXT, ' +
@@ -77,13 +82,13 @@ const database = {
         'FOREIGN KEY(prenup_record_id) REFERENCES pre_nuptial(record_id)' +
       ')'
 
-    const createChurchIncome =
-      'CREATE TABLE IF NOT EXISTS church_income (' +
-        'income_id INTEGER NOT NULL PRIMARY KEY AUTOINCREMENT,' +
+    const createDonationRecord = 
+      'CREATE TABLE IF NOT EXISTS donatations (' +
+        'donation_record_id INTEGER NOT NULL PRIMARY KEY AUTOINCREMENT,' +
         'member_id INTEGER NOT NULL,' +
         'type TEXT, ' +
         'amount REAL, ' +
-        'date DATE,' +
+        'date TEXT,' +
         'FOREIGN KEY(member_id) REFERENCES members(member_id) ' +
       ')'
 
@@ -103,13 +108,11 @@ const database = {
         'bap_reg_id INTEGER,' +
         'wedding_reg_id INTEGER,' +
         'prenup_record_id INTEGER,' +
+        'person_id INTEGER NOT NULL,' +
         'member_status TEXT,' +
-        'first_name TEXT,' +
-        'middle_name TEXT,' +
-        'last_name TEXT,' +
         'civil_status TEXT,' +
         'age INTEGER,' +
-        'birthday DATE,' +
+        'birthday TEXT,' +
         'occupation TEXT,' +
         'workplace TEXT,' +
         'email TEXT,' +
@@ -119,7 +122,33 @@ const database = {
         'FOREIGN KEY(address_id) REFERENCES address(address_id),' +
         'FOREIGN KEY(bap_reg_id) REFERENCES bap_reg(reg_id), ' +
         'FOREIGN KEY(wedding_reg_id) REFERENCES wedding_reg(reg_id),' +
-        'FOREIGN KEY(prenup_record_id) REFERENCES pre_nuptial(record_id)' +
+        'FOREIGN KEY(prenup_record_id) REFERENCES pre_nuptial(record_id),' +
+        'FOREIGN KEY(person_id) REFERENCES people(person_id)' +
+      ')'
+
+    const createPerson = 
+      'CREATE TABLE IF NOT EXISTS people(' + 
+        'person_id INTEGER NOT NULL PRIMARY KEY AUTOINCREMENT, ' +
+        'member_id INTEGER,' +
+        'first_name TEXT,' +
+        'middle_name TEXT,' +
+        'last_name TEXT,' +
+        'FOREIGN KEY(member_id) references members(member_id)' +
+      ')'
+
+    const createAttendance = 
+      'CREATE TABLE IF NOT EXISTS attendance(' + 
+        'attendance_id INTEGER NOT NULL PRIMARY KEY AUTOINCREMENT, ' +
+        'person_id INTEGER NOT NULL,' +
+        'date TEXT,' +
+        'FOREIGN KEY(person_id) REFERENCES people(person_id)' +
+      ')'
+    
+    const createCouple = 
+      'CREATE TABLE IF NOT EXISTS couples(' + 
+      'couple_id INTEGER NOT NULL PRIMARY KEY AUTOINCREMENT, ' +
+      'female_id INTEGER NOT NULL,' +
+      'male_id INTEGER NOT NULL' +
       ')'
 
     db.prepare(createBapReg).run()
@@ -130,7 +159,10 @@ const database = {
     db.prepare(createWeddingReg).run()
     db.prepare(createAddress).run()
     db.prepare(createMembers).run()
-    db.prepare(createChurchIncome).run()
+    db.prepare(createDonationRecord).run()
+    db.prepare(createCouple).run()
+    db.prepare(createPerson).run()
+    db.prepare(createAttendance).run()
 
     db.close()
   },
