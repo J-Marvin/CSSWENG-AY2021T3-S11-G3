0fe6b1ed9ee48d2ff8695e9683de91b6d6d05338
J-Marvin <60575288+J-Marvin@users.noreply.github.com>
2021-07-26 13:42:57 +0800

    Added example to add data in object

--- controllers/indexController.js
+++ controllers/indexController.js
@@ -1,13 +1,15 @@
 const db = require('../models/db.js')
 const sqlite3 = require('better-sqlite3')
 const path = require('path')
+const member_fields = require('../models/members.js')
 
 const controller = {
   getIndex: function (req, res) {
 
-    let data = {
-      'workplace': "Manila"
-    }
+    let data = {}
+
+    
+    data[member_fields.WORKPLACE] = "Baguio"
 
     db.insertOne(db.tables.MEMBER_TABLE, data, function(result) {
       console.log(result)
