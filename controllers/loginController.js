e4f213c490da90af44cb009799aed0c6f8ea5e56
J-Marvin <60575288+J-Marvin@users.noreply.github.com>
2021-07-27 16:02:02 +0800

    Fixed login

--- controllers/loginController.js
+++ controllers/loginController.js
@@ -1,6 +1,5 @@
 const bcrypt = require('bcrypt')
 const db = require('../models/db')
-const async = require('async')
 
 const loginController = {
   getLoginPage: function (req, res) {
@@ -9,20 +8,23 @@ const loginController = {
   postLogIn: function (req, res) {
     const password = req.body.password
 
-    db.findAll(db.tables.ACCOUNT_TABLE, function (result) {
-      async.each(result, function (account, callback) {
-        bcrypt.compare(password, account.hashed_password, function (err, same) {
-          if (err) {
-            console.log(err)
-          } else if (same) {
-            res.send(account.level)
+    new Promise((resolve, reject) => {
+      db.findAll(db.tables.ACCOUNT_TABLE, '*', async function (result) {
+        let level = 0
+        for (let i = 0; i < result.length && level === 0; i++) {
+          const same = await bcrypt.compare(password, result[i].hashed_password)
+          if (same) {
+            level = result[i].level
           }
-        })
-      }, function (err) {
-        if (err) {
-          console.log(err)
         }
+        resolve(level)
       })
+    }).then((level) => {
+      res.send((level + ''))
+    }).catch((err) => {
+      if (err) {
+        res.send(err.message)
+      }
     })
   }
 }
