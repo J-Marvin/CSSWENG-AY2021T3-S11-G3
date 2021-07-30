e67380890fb27c0b4398725f8f73c3d2e7680980
francisCalimbas <francis_calimbas@dlsu.edu.ph>
2021-07-31 00:52:13 +0800

    Fixed prenup routes

--- public/scripts/addPrenup.js
+++ public/scripts/addPrenup.js
@@ -2,9 +2,9 @@ $(document).ready(function () {
   $('.btn-primary').click(function () {
     // if disabled, it is checked
     if ($('.container').is(':disabled')) {
-      $('#form_id').attr('action', '/createPrenup')
+      $('#form_id').attr('action', '/create_prenup')
     } else { // else prenup will be made to a member
-      $('.container').attr('action', '/createMemberPrenup')
+      $('.container').attr('action', '/create_prenup_member')
     }
   })
 })
