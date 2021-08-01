cde0ce7e821b77724a4b991c0f0f888d571f2177
J-Marvin <60575288+J-Marvin@users.noreply.github.com>
2021-08-01 15:28:42 +0800

    Fixed civil and member status

--- helpers/hbsHelper.js
+++ helpers/hbsHelper.js
@@ -13,6 +13,20 @@ const hbHelpers = {
     } else {
       return '<option>Male</option> <option selected>Female</option>'
     }
+  },
+  civilStatusSelection: function (civilStatus) {
+    if (civilStatus === 'Single') {
+      return '<option selected>Single</option> <option>Married</option>'
+    } else {
+      return '<option>Single</option> <option selected>Married</option>'
+    }
+  },
+  memberStatusSelection: function(memberStatus) {
+    if (memberStatus === 'Active') {
+      return '<option selected>Active</option> <option>Inactive</option>'
+    } else {
+      return '<option>Active</option> <option selected>Inactive</option>'
+    }
   }
 
 }
