bd421d47c77baacbdac71d5549b5a8eeb2f7bb9a
J-Marvin <60575288+J-Marvin@users.noreply.github.com>
2021-08-12 13:46:30 +0800

    Added dedicationController

---
+++ controllers/dedicationController.js
@@ -0,0 +1,15 @@
+const db = require('../models/db')
+const coupleFields = require('../models/couple')
+
+const dedicationController = {
+  getDedicationPage: function (req, res) {
+    res.send('Temp')
+  },
+
+  postAddDedication: function (req, res) {
+    data = {}
+    
+  }
+}
+
+module.exports = dedicationController
