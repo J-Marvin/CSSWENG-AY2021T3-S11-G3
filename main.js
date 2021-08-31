ceb929c00ded609c762a64fad3186f4b1fe8d7d9
J-Marvin <60575288+J-Marvin@users.noreply.github.com>
2021-09-01 00:45:31 +0800

    Added error handling in electron executable

--- main.js
+++ main.js
@@ -1,14 +1,37 @@
 // Testing
 
-const { app, BrowserWindow, globalShortcut } = require('electron')
+const { app, BrowserWindow, globalShortcut, dialog } = require('electron')
 const dotenv = require('dotenv')
 const path = require('path')
 const fse = require('fs-extra')
+const appPath = app.getAppPath()
+let logPath = 'logs'
+
+if (process.env.PORTABLE_EXECUTABLE_DIR !== undefined) {
+  logPath = path.join(process.env.PORTABLE_EXECUTABLE_DIR, '/logs')
+} else if (fse.existsSync(path.join(appPath, '../../logs'))) {
+  logPath = path.join(appPath, '../../logs')
+}
 
 dotenv.config({ path: path.join(__dirname, '.env') })
 
 const port = process.env.PORT
 const hostname = process.env.HOSTNAME
+
+if (!fse.existsSync(logPath)) {
+  fse.mkdirSync(logPath)
+}
+
+process.on('uncaughtException', function (error) {
+  const log = require('electron-log')
+  const errorLogPath = path.join(logPath, 'main.log')
+  log.transports.file.resolvePath = () => errorLogPath
+
+  log.error(error)
+  dialog.showErrorBox('Failed to start application', 'Something went wrong! A log of the error has been made and can be located at \n' + errorLogPath.replace(/\\/g, '\n'))
+  app.quit()
+})
+
 require('./app.js')
 
 let mainWindow
@@ -45,9 +68,4 @@ app.on('activate', function () {
   if (mainWindow === null) {
     createWindow()
   }
-})
-
-app.on('uncaughtException', function(error) {
-  const log = require('electron-log')
-  log.error(error)
 })
